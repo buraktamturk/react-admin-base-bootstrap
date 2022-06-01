b19a353d92878c45478a5a04a441a0bfb37cdde9
Burak Tamturk <buraktamturk@gmail.com>
2022-06-01 22:38:16 +0300

    Excel small btn

--- src/Components/ExcelExportButton.tsx
+++ src/Components/ExcelExportButton.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import {useExporter} from 'react-admin-base';
 import {Button, Col} from "reactstrap";
 
-export default function ExcelExportButton({name, header, params, map, extra}) {
+export default function ExcelExportButton({name, header, params, size, map, extra}) {
     const [ handleExport, loading ] = useExporter(header, params, map, extra);
 
-    return <Col><Button className="w-100 d-block" type="button" size="lg" color="success" outline disabled={!!loading} onClick={() => handleExport(name)}>
+    return <Col><Button className="w-100 d-block" type="button" size={size} color="success" outline disabled={!!loading} onClick={() => handleExport(name)}>
         {loading ? <i className="fas fa-spin fa-spinner"/> : <i className="fas fa-file-excel"/>}
     </Button></Col>
 }
