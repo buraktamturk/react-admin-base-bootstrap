6077ef7d3a77e6fb9bd0649ef31dc381cf275cd5
Burak Tamturk <buraktamturk@gmail.com>
2022-12-21 08:36:39 +0200

    updates

--- src/Components/TopProgressBar.tsx
+++ src/Components/TopProgressBar.tsx
@@ -2,16 +2,12 @@ import NProgress from 'nprogress';
 import React, { useEffect } from 'react';
 import { useAuth } from "react-admin-base";
 
-type TopProgressBarProps = {
-    children: React.ReactNode;
-};
-
-export default function TopProgressBar({ children }: TopProgressBarProps): React.ReactNode {
+export default function TopProgressBar() {
 	const [ api ] = useAuth();
 
 	useEffect(function() {
 		return api.set_hook(() => NProgress.start(), () => NProgress.done());
 	}, [api.set_hook]);
 
-	return children;
+	return null;
 }
